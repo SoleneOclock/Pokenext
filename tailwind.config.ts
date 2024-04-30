75388ff83011caf8d098953622cb26e5c134863c
SoleneOclock <solene@oclock.io>
2024-04-30 09:48:16 +0200

    creation d'un titre stylisé avec tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'pink': '#f0f',
+      'tahiti': '#3ab7bf',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
